e5cc8b997fd2c90b2dd5de127f1c706aa81c5cfb
Rithmatist <chrisdebont@protonmail.com>
2022-03-01 14:27:53 +0100

    Eind Project

--- Project/hello-cdk/app.py
+++ Project/hello-cdk/app.py
@@ -4,12 +4,12 @@ import os
 
 import aws_cdk as cdk
 
-from hello_cdk.hello_cdk_stack import CdkVpcStack
+from hello_cdk.Sentia_cdk_stack import CdkVpcStack
 from hello_cdk.Peering import PeeringStack
 from hello_cdk.iamusers import iamusers
 
 app = cdk.App()
-CdkVpcStack(app, "HelloCdkStack", env=cdk.Environment(account='600563666729', region='eu-central-1'))
+CdkVpcStack(app, "SentiaCdkStack", env=cdk.Environment(account='600563666729', region='eu-central-1'))
 # PeeringStack(app, "PeeringStack",env=cdk.Environment(account='600563666729', region='eu-central-1'))
 # iamusers(app, "iamusers",env=cdk.Environment(account='600563666729', region='eu-central-1'))
 
--- Project/hello-cdk/hello_cdk/Sentia_cdk_stack.py
+++ Project/hello-cdk/hello_cdk/Sentia_cdk_stack.py
@@ -1,5 +1,6 @@
 import aws_cdk as cdk
 from cdk_ec2_key_pair import KeyPair
+from aws_cdk.aws_s3_assets import Asset
 from aws_cdk import (
     Stack,
     aws_ec2 as ec2,
@@ -10,9 +11,6 @@ from aws_cdk import (
     aws_s3_deployment as s3deploy,
 )
 from constructs import Construct
-
-with open("./hello_cdk/user_data/user-data.sh") as f:
-    user_data = f.read()
 
 
 class CdkVpcStack(Stack):
@@ -30,8 +28,8 @@ class CdkVpcStack(Stack):
 
         s3deploy.BucketDeployment(
             self, "S3Deployment",
-            sources=[s3deploy.Source.asset("./hello_cdk/user_data")],
-            destination_bucket=bucket,
+            sources=[s3deploy.Source.asset("./hello_cdk/userdata")],
+            destination_bucket=bucket
         )
 
         vpc1 = ec2.Vpc(
@@ -115,6 +113,11 @@ class CdkVpcStack(Stack):
             allow_all_outbound=True
         )
 
+        Webserver_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "allow http access from the world")
+        Webserver_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443), "allow https access from the world")
+        Webserver_sg.add_ingress_rule(ec2.Peer.security_group_id(Management_sg.security_group_id),
+                                      ec2.Port.tcp(22), "allow ssh access from the management Security Group")
+
         # Create a key pair with lambda function that will store the public and private key in secrets manager
 
         mmkey = KeyPair(
@@ -141,10 +144,9 @@ class CdkVpcStack(Stack):
             machine_image=amzn_linux,
             security_group=Webserver_sg,
             vpc=vpc1,
-            role=role,
+            # user_data=ec2.UserData.custom(user_data),
             availability_zone="eu-central-1a",
             key_name=webkey.key_pair_name,
-            user_data=ec2.UserData.custom(user_data),
             block_devices=[ec2.BlockDevice(
                 device_name="/dev/xvda",
                 volume=ec2.BlockDeviceVolume.ebs(8,
@@ -153,6 +155,21 @@ class CdkVpcStack(Stack):
             )
             ]
         )
+        assets = Asset(self, "Asset",
+                       path="./userdata/userdata.sh"
+                       )
+
+        Local_path = Webserver.user_data.add_s3_download_command(
+            bucket=assets.bucket,
+            bucket_key=assets.s3_object_key,
+            region="eu-central-1",
+        )
+
+        Webserver.user_data.add_execute_file_command(
+            file_path=Local_path
+        )
+
+        assets.grant_read(Webserver.role)
 
         ManagementServer = ec2.Instance(
             self, "ManagementServer",
@@ -160,7 +177,6 @@ class CdkVpcStack(Stack):
             machine_image=windows,
             vpc=vpc2,
             security_group=Management_sg,
-            role=role,
             availability_zone="eu-central-1b",
             key_name=mmkey.key_pair_name,
             block_devices=[ec2.BlockDevice(
@@ -171,11 +187,6 @@ class CdkVpcStack(Stack):
             )
             ]
         )
-
-        Webserver_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(80), "allow http access from the world")
-        Webserver_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(443), "allow https access from the world")
-        Webserver_sg.add_ingress_rule(ec2.Peer.security_group_id(Management_sg.security_group_id),
-                                      ec2.Port.tcp(22), "allow ssh access from the management Security Group")
 
         web_backup_plan = bk.BackupPlan(
             self, "WebServerStorageBackupPlanEBS",
